1ecef9ab0c4bc2d931e3f5816e715bcc6032f494
Matias Lang <matias.la@exodus.io>
2023-03-28 15:34:13 -0300

    fix: add missing sodium-crypto.d.ts file

---
+++ packages/utils/src/sodium-crypto.d.ts
@@ -0,0 +1,15 @@
+declare module "@exodus/sodium-crypto" {
+  export function encryptAEAD(
+    message: Uint8Array,
+    secretKey: Uint8Array,
+    nonce: Uint8Array,
+    associatedData?: Uint8Array | null
+  ): Buffer;
+  export function decryptAEAD(
+    ciphertext: Uint8Array,
+    secretKey: Uint8Array,
+    nonce: Uint8Array,
+    associatedData?: Uint8Array | null
+  ): Buffer;
+};
+
