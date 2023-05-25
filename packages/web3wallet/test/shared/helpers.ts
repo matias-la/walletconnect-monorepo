635d906eeac58c1e54249ca0dc1c7ee6267745bc
Matias Lang <matias.la@exodus.io>
2023-05-25 19:27:26 -0300

    delete web3wallet packages
    it depended on @walletconnect/auth-clien, for which I couldn't find its
    source code to fork

--- packages/web3wallet/test/shared/helpers.ts
+++
@@ -1,7 +0,0 @@
-import { ICore } from "@exodus/walletconnect-types";
-
-export async function disconnect(core: ICore) {
-  if (core.relayer.connected) {
-    await core.relayer.transportClose();
-  }
-}
